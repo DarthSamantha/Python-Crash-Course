7ad3e53873382dc725ca04622e2631a8b07d80e9
Darth Samantha <samanthajademillar@gmail.com>
2021-12-18 15:29:51 +1300

    Updates

--- Chapter 3 - Lists/list_basics.py
+++ Chapter 3 - Lists/list_basics.py
@@ -7,3 +7,20 @@ print(food[-1])
 message = f"My favourie from this list is {food[1].title()}"
 print(message)
 
+food[0] = 'twirl'
+print(food)
+
+
+
+food.append('banana')
+print(food)
+
+food.insert(1, 'cookie')
+print(food)
+
+del food[4]
+print(food)
+
+popped_food = food.pop()
+print(food)
+print(popped_food)
