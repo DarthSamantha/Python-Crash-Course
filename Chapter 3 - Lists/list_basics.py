bad3373e3ef55505362ef2cc0180cdbc5422c678
DarthSamantha <samanthajademillar@gmail.com>
2021-12-18 13:56:55 +1300

    Updated with Chapter 3

---
+++ Chapter 3 - Lists/list_basics.py
@@ -0,0 +1,9 @@
+food = ['chocolate', 'apple pie', 'cheese', 'pasta']
+print(food)
+
+print(food[0])
+print(food[-1])
+
+message = f"My favourie from this list is {food[1].title()}"
+print(message)
+
