60649111acc11e4315241074b9173ab66f6c5cfd
Darth Samantha <samanthajademillar@gmail.com>
2022-01-20 19:15:02 +1300

    sams super learning

---
+++ Chapter 5 -If Statements/basics_for_if_statements.py
@@ -0,0 +1,7 @@
+cars = ['audi', 'bmw', 'mazda', 'toyota']
+for car in cars:
+    if car == 'audi':
+        print(f"This car is an {car.title()}")
+    else:
+        print("This car is not an Audi")
+
