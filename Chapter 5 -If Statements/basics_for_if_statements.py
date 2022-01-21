8968cddc2d65edf4bac98de34490328d06bf29d4
DarthSamantha <samanthajademillar@gmail.com>
2022-01-22 10:52:32 +1300

    Finished Chapter 5 - if statements

--- Chapter 5 -If Statements/basics_for_if_statements.py
+++ Chapter 5 -If Statements/basics_for_if_statements.py
@@ -22,3 +22,61 @@ if 'apple pie' not in food:
     print("Apple pie should be in the list")
 
 game_active = True
+
+age = 12
+if age > 20:
+    print("Age is greater than 20")
+elif age < 15:
+    print("Age is less than 15")
+else:
+    print("Age is between 15 and 20")
+
+age = 12
+if age < 5:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+
+print(f"Your admission price is ${price}.")
+#Else is option, it's a catch all
+
+#To test multiple conditions use if statements
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding Mushrooms")
+if 'pepperoni' in requested_toppings:
+    print("Adding Pepperoni")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese")
+
+print("Finished making you pizza")
+
+requested_toppings = ['mushrooms', 'extra cheese', 'mushrooms', 'green peppers']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("Finished making you pizza.")
+
+#Checking that a list is not empty
+requested_toppings = []
+if requested_toppings:
+#^ Python will return true if the list is not empty 
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_toppings}.")
+    print("Finished making you pizza")
+else:
+    print("Are you sure you want a plan pizza?")
+
+available_toppings = ['mushrooms', 'olives', 'extra cheese', 'pineapple']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping} to pizza.")
+    else:
+         print(f"Sorry that is not an available topping.")
+print("Finished making pizza.")
+
---
+++ Chapter 5 -If Statements/exercises_with_if_statements.py
@@ -0,0 +1,13 @@
+alien_colour = 'purple'
+if alien_colour == 'green':
+    print("Congrats you've just earned 5 points")
+
+alien_colour = 'green'
+if alien_colour == 'green':
+    print("Congrats you've just earned 5 points")
+
+alien_colour = 'purple'
+if alien_colour == 'green':
+    print("Congrats you just earned 5 points")
+else:
+    print("Congrats you just earned 10 points")
