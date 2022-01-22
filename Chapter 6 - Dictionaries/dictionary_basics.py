f6c56e52c5cb2bd814e81d8ce4287bb267429a5a
DarthSamantha <samanthajademillar@gmail.com>
2022-01-22 17:43:26 +1300

    Chapter 6 updates

--- Chapter 6 - Dictionaries/dictionary_basics.py
+++ Chapter 6 - Dictionaries/dictionary_basics.py
@@ -15,4 +15,57 @@ print(f"The alien is now the colour {alien_0['colour']}")
 del alien_0['points']
 print(alien_0)
 
+# get() method requires a key as a first argument
+# 2nd optional argument is if value doesn't exist
+
+point_value = alien_0.get('points', 'No point value assigned')
+print(point_value)
+
+rachels_dictionary = {
+    'first_name': 'Rachel',
+    'last_name': 'Millar',
+    'age': '25',
+    'city': 'Auckland'
+}
+
+print(rachels_dictionary['age'])
+print(f"{rachels_dictionary['first_name']} lives in {rachels_dictionary['city']}")
+
+for key, value in rachels_dictionary.items():
+    print(f"\nKey: {key}")
+    print(f"Value: {value}")
+
+print(f"\n{rachels_dictionary.items()}")
+print("\n")
+
+for key in rachels_dictionary.keys():
+    print(key)
+
+for key in rachels_dictionary:
+    print(key)
+
+favourite_foods = {
+    'Rachel': 'chips',
+    'Debbie': 'chocolate',
+    'David': 'chips',
+    'Random': 'carrots'}
+
+family = ['Rachel', 'Debbie', 'David']
+
+for name in favourite_foods:
+    print(f"Hi, {name}")
+    
+    if name in family:
+        food = favourite_foods[name].title()
+        print(f"\t{name}, I see you love {food}")
+
+
+
+
+
+
+
+
+
+
 
