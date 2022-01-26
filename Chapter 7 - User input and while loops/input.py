8914d3968848e9a20813169213db2221d0f45fb2
DarthSamantha <samanthajademillar@gmail.com>
2022-01-26 18:06:03 +1300

    Chapter 7 added

---
+++ Chapter 7 - User input and while loops/input.py
@@ -0,0 +1,21 @@
+message = input("Tell me something and i will repeat it: ")
+print(message)
+
+prompt = "If the prompt is longer than one line "
+prompt += "then pass to a variable - "
+prompt += "What is your name? "
+
+name=input(prompt)
+print(name)
+
+age = input("How old are you? ")
+age = int(age)
+print(age >= 18)
+
+# Modulo Operator % divides and returns the remainder
+
+print(4 % 5)
+
+
+
+
